b12a3c8a57b4d4940a9423970d384ffb753dc4b9
TeamGodLike4 <alokhr04@outlook.com>
2024-06-09 12:07:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,53 +1,58 @@
-import express from 'express';
-import { createServer } from 'http';
-import { toBuffer } from 'qrcode';
-import fetch from 'node-fetch';
+import express from 'express'
+import { createServer } from 'http'
+import path from 'path'
+import { Socket } from 'socket.io'
+import { toBuffer } from 'qrcode'
+import fetch from 'node-fetch'
 
-function connect(conn) {
-    const app = express();
-    const server = createServer(app);
-    const PORT = process.env.PORT || 3000;
-    let _qr = 'invalid';
+function connect(conn, PORT) {
+    let app = global.app = express()
+    console.log(app)
+    let server = global.server = createServer(app)
+    // app.use(express.static(path.join(__dirname, 'views')))
+    let _qr = 'invalid'
 
-    conn.ev.on('connection.update', ({ qr }) => {
-        if (qr) _qr = qr;
-    });
+    conn.ev.on('connection.update', function appQR({ qr }) {
+        if (qr) _qr = qr
+    })
 
-    app.get('/', async (req, res) => {
-        try {
-            res.setHeader('content-type', 'image/png');
-            res.end(await toBuffer(_qr));
-        } catch (error) {
-            console.error('Error generating QR code:', error);
-            res.status(500).send('Internal Server Error');
-        }
-    });
+    app.use(async (req, res) => {
+        res.setHeader('content-type', 'image/png')
+        res.end(await toBuffer(_qr))
+    })
+
+    // let io = new Socket(server)
+    // io.on('connection', socket => {
+    //     let { unpipeEmit } = pipeEmit(conn, socket, 'conn-')
+    //     socket.on('disconnect', unpipeEmit)
+    // })
 
     server.listen(PORT, () => {
-        console.log('App is listening on port', PORT);
-        if (process.env.KEEPALIVE) keepAlive();
-    });
+        console.log('App listened on port', PORT)
+        if (opts['keepalive']) keepAlive()
+    })
 }
 
 function pipeEmit(event, event2, prefix = '') {
-    const oldEmit = event.emit;
+    let old = event.emit
     event.emit = function (event, ...args) {
-        oldEmit.call(event, event, ...args);
-        event2.emit(prefix + event, ...args);
-    };
+        old.emit(event, ...args)
+        event2.emit(prefix + event, ...args)
+    }
     return {
         unpipeEmit() {
-            event.emit = oldEmit;
+            event.emit = old
         }
-    };
+    }
 }
 
 function keepAlive() {
-    const url = `https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`;
-    if (/(\/\/|\.)undefined\./.test(url)) return;
+    const url = `https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`
+    if (/(\/\/|\.)undefined\./.test(url)) return
     setInterval(() => {
-        fetch(url).catch(console.error);
-    }, 5 * 1000 * 60);
+        fetch(url).catch(console.error)
+    }, 5 * 1000 * 60)
 }
+
 
 export default connect;
